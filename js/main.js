664d097fe094bbbc9200e09d90c258dd2d94e6cf
arat <aratislamsafol@gmail.com>
2021-09-10 02:01:34 +0600

    navbar transparent problem & image shape isn't control-[banner_image] class

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.typing', {
+    strings: ['Designer.','Professional Coder.', 'Developer.'],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true,
+    loopCount: Infinity,
+    smartBackspace: true, 
+
+  });
